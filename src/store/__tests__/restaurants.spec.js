2a46bf57447c197db70960474320d6f3e93a7273
Josh Justice <josh.justice@accesscfa.com>
2020-04-02 07:54:51 -0400

    Start out loading false

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -4,6 +4,20 @@ import restaurantsReducer from '../restaurants/reducers';
 import {loadRestaurants} from '../restaurants/actions';
 
 describe('restaurants', () => {
+  describe('initially', () => {
+    it('does not have the loading flag set', () => {
+      const initialState = {};
+
+      const store = createStore(
+        restaurantsReducer,
+        initialState,
+        applyMiddleware(thunk),
+      );
+
+      expect(store.getState().loading).toEqual(false);
+    });
+  });
+
   describe('loadRestaurants action', () => {
     describe('while loading', () => {
       it('sets a loading flag', () => {
