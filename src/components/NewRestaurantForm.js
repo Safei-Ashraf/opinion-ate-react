7518528fc8e68693d4bfa37f2150bb49bbe56585
Josh Justice <josh.justice@accesscfa.com>
2020-04-03 20:03:51 -0400

    Add text field

---
+++ src/components/NewRestaurantForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+export const NewRestaurantForm = () => {
+  return (
+    <form>
+      <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
+    </form>
+  );
+};
+
+export default NewRestaurantForm;
