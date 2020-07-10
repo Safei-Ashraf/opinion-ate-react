974c7cdf4cd196eb1d9f0a6d8d134d8b8cf74362
Josh Justice <josh.justice@accesscfa.com>
2020-07-10 06:54:54 -0400

    Add axe disabled

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// if (process.env.NODE_ENV !== 'production') {
+//   var axe = require('react-axe');
+//   axe(React, ReactDOM, 1000);
+// }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
